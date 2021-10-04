eb04f4dbc85be409a780be6cd7388c891ba457e8
mehesultana <sultanamehe01@gmail.com>
2021-10-04 13:44:45 +0600

    home update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,30 +1,75 @@
-import Button from '@restart/ui/esm/Button';
 import React from 'react';
 import { Card, CardGroup, Container } from 'react-bootstrap';
-import img from '../../images/MySchoolBanner.png';
 import img1 from '../../images/photo1.svg';
 import img2 from '../../images/photo2.svg';
 import img3 from '../../images/photo3.svg';
-
+import img4 from '../../images/home2.png';
+import img5 from '../../images/course.png';
+import img6 from '../../images/course2.png';
+import img7 from '../../images/course3.png';
 import './Home.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
 	return (
 		<Container>
-			<div>
-				<Card className="bg-dark text-white ">
-					<Card.Img src={img} alt="Card image" class="opacity-25" />
-					<Card.ImgOverlay>
-						<Card.Title>Learn a language for real life</Card.Title>
-						<Card.Text>Phrases useful in everyday life. Taught with video clips of real locals.</Card.Text>
-						<br />
-						<Button variant="warning">Get Started</Button>
-					</Card.ImgOverlay>
-				</Card>
+			<div id="carouselExampleSlidesOnly" class="carousel slide bg-success" data-ride="carousel ">
+				<div class="carousel-inner">
+					<div class="carousel-item active">
+						<div class="row align-items-center">
+							<div class="col-md-6 mt-5 mb-5 order-md-1 pb-5 banner-text">
+								<h1 class="text-white">Learn a language for real life</h1>
+								<p class="text-white">Phrases useful in everyday life. Taught with video clips of real locals.</p>
+
+								<button type="button" class="btn btn-warning">
+									Get Started
+								</button>
+							</div>
+							<div class="col-md-6 align-items-center order-2 order-md-2">
+								<img src={img4} class="d-block w-50" alt="..." />
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
+
+			{/* courses  */}
+
+			<div className="courses-part">
+				<h1>Our Popular Courses</h1>
+				<br />
+				<CardGroup>
+					<Card>
+						<Card.Img variant="top" src={img5} className="h-10" />
+						<Card.Body>
+							<Card.Title>Arabic Through the Quran exercises</Card.Title>
+						</Card.Body>
+					</Card>
+					<Card>
+						<Card.Img variant="top" src={img6} className="h-10" />
+						<Card.Body>
+							<Card.Title>Learn French online with My School</Card.Title>
+						</Card.Body>
+					</Card>
+					<Card>
+						<Card.Img variant="top" src={img7} className="h-20" />
+						<Card.Body>
+							<Card.Title>Learn German online with My School</Card.Title>
+						</Card.Body>
+					</Card>
+				</CardGroup>
+				<br />
+				<button type="button" class="btn btn-warning">
+					Explore More <FontAwesomeIcon icon={faArrowAltCircleRight} />
+				</button>
+			</div>
+
+			{/* about my school */}
+
 			<div className="middle-part">
 				<h1>Why My School?</h1>
-				<CardGroup className="card ">
+				<CardGroup>
 					<Card>
 						<Card.Img variant="top" src={img1} className="card-img" />
 						<Card.Body>
