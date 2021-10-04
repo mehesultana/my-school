d57bdb808cc250a518915b9a90821f03f3a8d3d5
mehesultana <sultanamehe01@gmail.com>
2021-10-04 18:35:45 +0600

    navbar update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
 	return (
-		<div>
-			<Navbar bg="dark" variant="dark">
-				<Container>
-					<Navbar.Brand to="/home">My School</Navbar.Brand>
-					<Nav className="me-auto">
-						<NavLink to="/home">Home</NavLink>
-						<NavLink to="/courses">Courses</NavLink>
-						<NavLink to="/blog">Blog</NavLink>
-						<NavLink to="/contact">Contact Us</NavLink>
-						<NavLink to="/signup">Sign Up</NavLink>
-					</Nav>
-				</Container>
-			</Navbar>
+		<div className="header">
+			<h1 to="/home">My School</h1>
+			<hr />
+			<Nav className="navbar">
+				<NavLink to="/home">Home</NavLink>
+				<NavLink to="/courses">Courses</NavLink>
+				<NavLink to="/blog">Blog</NavLink>
+				<NavLink to="/contact">Contact Us</NavLink>
+				<NavLink to="/signup">Sign Up</NavLink>
+			</Nav>
 		</div>
 	);
 };
